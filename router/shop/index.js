6bc5bafc0eba9c3a959d3eaa17ff1ffeaaa888a6
NBADigital <programmer@nbadigitalworlds.com>
2024-02-09 21:07:48 +0700

    up

--- router/shop/index.js
+++ router/shop/index.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
-const member = require("../../controllers/Member/member.controller")
-const authShop = require("../../lib/auth-shop")
+const member = require("../../controllers/Member/member.controller");
+const authShop = require("../../lib/auth-shop");
 
-
-router.post("/genPublicToken",member.genPublicToken)
-router.post("/regisMember",authShop,member.create)
-router.get("/memberShop/:tel", authShop,member.CheckInvit)
-
+router.post("/genPublicToken", member.genPublicToken);
+router.post("/regisMember", member.create);
+router.get("/memberShop/:tel", authShop, member.CheckInvit);
 
 module.exports = router;
